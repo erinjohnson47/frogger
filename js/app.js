6e207b32459a1c8f817c785dfd2003c5963cbb6c
Erin Johnson <erinjohnson47@gmail.com>
2019-07-09 14:06:45 -0700

    frog moves up & left logic complete

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const log = document.createElement('div');
 const logImg = document.createElement('img');
     logImg.id='log';
     logImg.src = "images/log.png";
+    logImg.width = "110px";
+    logImg.height = "50px";
     log.appendChild(logImg);
 const car = document.createElement('div');
 car.setAttribute('id','car');
@@ -13,41 +15,37 @@ const frogImg = document.createElement('img');
     frogImg.src="images/frogger.png";
     frog.appendChild(frogImg);
 
+const row = document.querySelectorAll(".row")
+
 const playBtn = document.querySelector('#start');
 
 playBtn.addEventListener("click", () => {
     game.start();
     game.frog();
 })
-
-document.addEventListener('keydown', (e) => {
+const key = document.addEventListener('keydown', (e) => {
     e.preventDefault(); //stops the page from scrolling instead of the frog moving
     console.log(`Key ${e.key} was pressed.`)
     switch (e.key) {
         case "ArrowLeft":
-            game.frog();
+            game.move(e.key);
             break; //what is this - it's in a lot of the code????
         case "ArrowRight":
-            game.frog();
+            game.move(e.key);
             break;
         case "ArrowUp":
-            game.frog();
+            game.move(e.key);
             break;
         case "ArrowDown":
-            game.frog();
+            game.move(e.key);
             break;
 }
 });
 
-//arrow keys in js
-// left arrow	37
-// up arrow	38
-// right arrow	39
-// down arrow	40
-
 const game = {
-    lives: 3, /*code here to show 3 frog icons instead of the number 3*/
+    lives: 6, /*code here to show 3 frog icons instead of the number 3*/
     timer: 60,
+    currentPosition: [10,5],
     gameboard: [
         ['b','l','b','l','b','l','b','l','b','l','b'],
         ['w','w','w','w','w','w','w','w','w','w','w'],
@@ -63,7 +61,14 @@ const game = {
     ],
     start() {
         //this is the starting position for the frog
+        let y = this.currentPosition[0];
+        let x = this.currentPosition[1];
         this.gameboard[10][5] = 'gF'
+        if (this.gameboard[y][x].includes("F")){
+            row[y].children[x].appendChild(frogImg)
+                // console.log(row[].children[j])
+                // console.log(frog);
+        }
 
     },
     frog() {
@@ -75,39 +80,72 @@ const game = {
        //wLF = frog on Log on water
        //lF = frog on lilipad - COMPLETE - round WON
        //bF = frog in bush - DEAD - NOT SAFE
-       const row = document.querySelectorAll(".row")
-        for (let i=0; i<this.gameboard.length; i++) {
-            for (let j=0; j<this.gameboard[i].length; j++){
-                if (this.gameboard[i][j].includes("F")){
-                    row[i].children[j].appendChild(frogImg)
-                    // console.log(row[i].children[j])
-                    // console.log(frog);
-                }
-            
-            // if (this.gameboard[i][i] === 'gF' || this.gameboard[i][i] === 'rF' ||this.gameboard[i][i] === 'wF' || this.gameboard[i][i] === 'rCF' || this.gameboard[i][i] === 'wLF' || this.gameboard[i][i] === 'lF' || this.gameboard[i][i] === 'bF') {
+    //    let y = this.currentPosition[0];
+    //    let x = this.currentPosition[1];
+    //     for (let i=0; i<this.gameboard.length; i++) {
+    //         for (let j=0; j<this.gameboard[i].length; j++){
+    //             if (this.gameboard[y][x].includes("F")){
+    //                 row[y].children[x].appendChild(frogImg)
+    //                     console.log(row[i].children[j])
+    //                     console.log(frog);
+    //         }
+    //         }
+ // if (this.gameboard[i][i] === 'gF' || this.gameboard[i][i] === 'rF' ||this.gameboard[i][i] === 'wF' || this.gameboard[i][i] === 'rCF' || this.gameboard[i][i] === 'wLF' || this.gameboard[i][i] === 'lF' || this.gameboard[i][i] === 'bF') {
             //need to add logic that when arrow key is pressed, F is moved to string in the direction of pressed key.  new string is created - div matching string is assigned.
             //https://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
             //     }
-            }   
+            // }   
+    
+    },
+    move(key) {
+        let y = this.currentPosition[0];
+        let x = this.currentPosition[1];
+
+        if (key === "ArrowUp" && this.gameboard[y-1]) {
+            this.gameboard[y][x]= this.gameboard[y][x].slice(0,this.gameboard[y][x].length-1);
+            this.gameboard[y-1][x] += 'F';
+            this.currentPosition = [y-1, x];
+            row[y-1].children[x].appendChild(frogImg);
+        } else if (key === "ArrowLeft" && this.gameboard[y][x-1]) {
+            this.gameboard[y][x]= this.gameboard[y][x].slice(0,this.gameboard[y][x].length-1);
+            this.gameboard[y][x-1] += 'F';
+            this.currentPosition = [y, x-1];
+            row[y].children[x-1].appendChild(frogImg);
+        } else if (key === "ArrowDown" && this.gameboard[y+1]) {
+
+            this.gameboard[y+=1][x]+'F';
+        } else if (key === "ArrowRight" && this.gameboard[y][x+1]) {
+            
+            this.gameboard[y][x+=1]+'F';
         }
-    },
-    car() {
-        //logic here that moves cars across the screen at varying speends in alternating directions
-        //cars in row 6 should move left slowly
-        //cars in row 7 should move left quickly
-        //cars in row 8 should move right quickly
-        //cars in row 9 should move right slowly
-        this.gameboard[9][9] = 'rC';
-        this.gameboard[9][9] = 'rC';
-    },
-    log() {
-        //logic here that moves logs across the screen at random speeds in alternating directions
-        //logs in row 1 should move right, slowly
-        //logs in row 2 should move left quickly
-        //logs in row 3 should move left slowly
-        //logs in row 4 should move right quickly
-        this.gameboard[2][2] = 'wL';
-        this.gameboard[2][3] = 'wL';
-        this.gameboard[2][4] = 'wL';
+            
+                // console.log(row[i].children[j])
+                // console.log(frog);
     }
+    // car() {
+    //     //logic here that moves cars across the screen at varying speends in alternating directions
+    //     //cars in row 6 should move left slowly
+    //     //cars in row 7 should move left quickly
+    //     //cars in row 8 should move right quickly
+    //     //cars in row 9 should move right slowly
+    //     this.gameboard[9][9] = 'rC';
+    //     this.gameboard[9][9] = 'rC';
+    // },
+    // log() {
+    //  //logic here that moves logs across the screen at random speeds in alternating directions
+    // //logs in row 1 should move right, slowly
+    // //logs in row 2 should move left quickly
+    // //logs in row 3 should move left slowly
+    // //logs in row 4 should move right quickly
+    // this.gameboard[2][2] = 'wL';
+    // this.gameboard[2][3] = 'wL';
+    // this.gameboard[2][4] = 'wL';
+    // for (let i=0; i<this.gameboard.length; i++) {
+    //     for (let j=0; j<this.gameboard[i].length; j++){
+    //         if (this.gameboard[i][j].includes("wL")){
+    //             row[i].children[j].appendChild(logImg);
+    //         }
+    //     }
+    // }
+    // }
 }
